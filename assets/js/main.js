f0ff490a3273033e33794c83896ce75b229df295
Yuri Machado <ymachado1995@gmail.com>
2024-07-26 02:02:06 -0300

    feat: Add event listener for keydown events in main.js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+import { showOptionsMenu, observeCommand } from "./menuConstruct.js";
+import { observeContentDiv } from "./ensureDivNotEmpty.js";
+import ensurePlaceholderBehavior from "./ensurePlaceholder.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const editableDiv = document.querySelector('.content-body-editable');
+
+    observeContentDiv();
+    ensurePlaceholderBehavior();
+
+    const handleKeyDown = (event) => {
+        observeCommand(event);
+    };
+
+    editableDiv.addEventListener('keydown', handleKeyDown);
+});
