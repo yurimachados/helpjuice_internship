ddf0b58f911c8920f6bdc2170bca15906e572e20
Yuri Machado <ymachado1995@gmail.com>
2024-07-26 03:57:27 -0300

    chore: improve menu construct functionalities

--- assets/js/menuConstruct.js
+++ assets/js/menuConstruct.js
@@ -1,17 +1,22 @@
 import textTypes from "./textTypes.js";
 import { setElementTextType } from "./elementMutations.js";
+import { createPlaceholderObserver } from "./ensurePlaceholder.js";
 
-console.log('Menu importado')
 const editableDiv = document.querySelector('.content-body-editable');
 const optionsMenu = document.getElementById('options-menu');
 const optionsUl = optionsMenu.querySelector('ul');
 let awaitingNumberInput = false;
+let menuClickFlag = null;
 
+/**
+ * Function to construct the menu options based on the text types
+ * @returns {void}
+ */
 const constructMenuOptions = () => {
     const existingTextTypes = optionsUl.querySelectorAll('li');
     existingTextTypes.forEach(li => li.remove());
 
-    Object.entries(textTypes).forEach(([index, { tag, placeholder }]) => {
+    Object.entries(textTypes).forEach(([index, { tag, placeholder, name }]) => {
         const menuItem = document.createElement('li');
         menuItem.classList.add('options-menu-item');
         menuItem.setAttribute('data-tag', tag);
@@ -20,26 +25,41 @@ const constructMenuOptions = () => {
         menuItem.innerHTML = `
             <img src="assets/svg/T.svg">
             <div>
-                <h4>${placeholder}</h4>
+                <h4>${name}</h4>
                 <p>Shortcut: type # + space</p>
             </div>
         `;
         menuItem.addEventListener('click', () => {
-            createNewElement(index, preCommandText);
+            menuClickFlag = index;
+            if (menuClickFlag !== null) {
+                setElementTextType(menuClickFlag, lastElementLocation, lastElementText);
+                menuClickFlag = null;
+            }
             hideOptionsMenu();
         });
 
         optionsUl.appendChild(menuItem);
     });
-    console.log('menu construido')
+    let filteredKeySpan = document.getElementById('filtered-key-count');
+    if (filteredKeySpan) {
+        filteredKeySpan.textContent = Object.keys(textTypes).length.toString(); // Correção aqui
+    }
 }
 
-constructMenuOptions();
 
 const hideOptionsMenu = () => {
     optionsMenu.style.display = 'none';
 };
 
+/**
+ * Function to show the options menu
+ * @returns {void}
+ * @param {HTMLElement} currentElement - The current element
+ * @param {string} preCommandText - The text before the command
+ * @param {string} text - The text of the current element
+ * @param {string} textType - The type of text
+ * @param {number} currentId - The id of the current element
+ */
 const showOptionsMenu = () => {
     setTimeout(() => {
         const selection = window.getSelection();
@@ -63,12 +83,23 @@ const showOptionsMenu = () => {
 }
 
 /**
- * Function to observe commands and execute actions
- * 
+ * function to observe the command input and trigger the command
  * @param {KeyboardEvent} e - The keyboard event
+ * @param {HTMLElement} currentElement - The current element
+ * @param {string} preCommandText - The text before the command
+ * @param {HTMLElement} lastElementLocation - The last element location
+ * @param {string} lastElementText - The last element text
+ * @param {boolean} awaitingNumberInput - The flag for awaiting number input
+ * @param {number} menuClickFlag - The flag for the menu click
+ * @param {HTMLElement} editableDiv - The editable div
+ * @param {HTMLElement} optionsMenu - The options menu
+ * @param {HTMLElement} optionsUl - The options menu ul
+ * @param {HTMLElement} menuItem - The menu item
  * @returns {void}
  */
 let preCommandText = '';
+let lastElementLocation = null;
+let lastElementText = null;
 const observeCommand = (e) => {
 
     let currentSelection = window.getSelection().anchorNode;
@@ -90,11 +121,16 @@ const observeCommand = (e) => {
 
     if (e.key === 'Enter') {
         e.preventDefault();
-        console.log('enter')
         commandEnter(currentElement);
     }
 
+    if (e.key === 'Backspace' && currentElement.textContent === '<br>') {
+        currentElement.innerHTML = '';
+    }
+
     if (e.key === '/') {
+        lastElementLocation = currentElement;
+        lastElementText = currentElement.textContent;
         let text = currentElement.textContent;
         preCommandText = text;
         if (optionsMenu) {
@@ -105,8 +141,7 @@ const observeCommand = (e) => {
         }
     } else if (awaitingNumberInput && !isNaN(e.key) && e.key !== ' ') {
         e.preventDefault();
-        const contentText = currentDiv.querySelector('.content-text');
-        setElementTextType(parseInt(e.key), contentText, preCommandText);
+        setElementTextType(parseInt(e.key), currentElement, preCommandText);
 
         if (commandActive) {
             commandActive.classList.remove('command-active');
@@ -128,6 +163,14 @@ const observeCommand = (e) => {
     }
 }
 
+/**
+ * Function to create a new element after the current element
+ * @param {HTMLElement} currentElement - The current element 
+ * @param {number} currentId - The id of the current element 
+ * @param {string} currentElementText - The text of the current element
+ * @param {string} textType - The type of text to be created
+ * @returns {void}
+ */
 const commandEnter = (currentElement) => {
     if (currentElement.nodeType === Node.TEXT_NODE) {
         currentElement = currentElement.parentElement;
@@ -140,12 +183,12 @@ const commandEnter = (currentElement) => {
 
     let currentElementText = currentElement.innerHTML;
 
-    if (currentElementText === '' || currentElementText === '<br>') {
+    if (currentElementText.trim() === '' || currentElementText === '<br>') {
         console.warn('Current element is empty. Cannot create a new element.');
         return
     }
 
-    let textType = textTypes[4];
+    let textType = textTypes[3];
     let newElement = document.createElement(textType.tag);
     newElement.setAttribute('data-placeholder', textType.placeholder);
 
@@ -154,10 +197,10 @@ const commandEnter = (currentElement) => {
     newElement.setAttribute('data-id', (newId));
 
     newElement.classList.add('content-text');
-    newElement.innerHTML = '<br>';
 
     currentElement.insertAdjacentElement('afterend', newElement);
 
+    createPlaceholderObserver(newElement);
     setTimeout(() => {
         newElement.focus();
         const range = document.createRange();
@@ -169,17 +212,5 @@ const commandEnter = (currentElement) => {
     }, 0);
 }
 
-function simulateKeyDownArrow() {
-    const event = new KeyboardEvent('keydown', {
-        key: 'ArrowDown',
-        code: 'ArrowDown',
-        keyCode: 40,
-        which: 40,
-        bubbles: true
-    });
-    document.dispatchEvent(event);
-}
-
+constructMenuOptions();
 export { showOptionsMenu, hideOptionsMenu, observeCommand };
-// Path: assets/js/createElement.js
-// Compare this snippet from assets/js/textTypes.js:
