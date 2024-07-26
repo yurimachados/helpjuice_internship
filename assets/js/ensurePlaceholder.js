7eaebc0c02dfc2934df436216778351c2775f051
Yuri Machado <ymachado1995@gmail.com>
2024-07-26 03:51:58 -0300

    feat: Add ensurePlaceholder.js file to observe and ensure content text elements have a placeholder when empty

---
+++ assets/js/ensurePlaceholder.js
@@ -0,0 +1,64 @@
+/**
+ * Ensure placeholder behavior for content text elements 
+ * @returns {void}
+ * @export
+ */
+export function ensurePlaceholderBehavior(){
+    console.log('ensurePlaceholderBehavior');
+    const elements = document.querySelectorAll('.content-text');
+
+    if (elements) {
+        elements.forEach((element) => {
+            createPlaceholderObserver(element);
+        });
+    }
+}
+
+
+/**
+ * Function to create a MutationObserver for a content text element
+ * @param {HTMLElement} element - The content text element
+ * @returns {void}
+ * @export
+ */
+export function createPlaceholderObserver (element) {
+    checkAndTogglePlaceholder(element);
+
+    const observer = new MutationObserver(() => {
+        checkAndTogglePlaceholder(element);
+        checkBr(element);
+    });
+
+    const config = { childList: true, subtree: true, characterData: true };
+
+    observer.observe(element, config);
+}
+
+/**
+ * Function to check if the element is empty and toggle the placeholder class
+ * @param {HTMLElement} element - The element to check
+ * @returns {void}
+ */
+const checkAndTogglePlaceholder = (element) => {
+    if (element.textContent.trim() === '') {
+        element.classList.add('placeholder');
+    } else {
+        element.classList.remove('placeholder');
+    }
+};
+
+/**
+ * Function to check if the element has br tags and remove them
+ * @param {HTMLElement} element - The element to check
+ * @returns {void}
+ */
+const checkBr = (element) => {
+    console.log('checkBr');
+    let brs = element.querySelectorAll('br');
+    if(brs){
+        console.log('brs', brs);
+        brs.forEach((br) => {
+            br.remove();
+        });
+    }
+}
