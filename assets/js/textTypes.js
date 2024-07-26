ebe22c60b95e84810e423466f31882e0901d7954
Yuri Machado <ymachado1995@gmail.com>
2024-07-26 03:48:05 -0300

    feat: Update textTypes.js with improved placeholder and name for text types

--- assets/js/textTypes.js
+++ assets/js/textTypes.js
@@ -1,8 +1,7 @@
 const textTypes = {
-    1: { id: 1, tag: "h1", placeholder: "Heading 1" },
-    2: { id: 2, tag: "h2", placeholder: "Heading 2" },
-    3: { id: 3, tag: "strong", placeholder: "Bold" },
-    4: { id: 4, tag: "p", placeholder: "Type / for blocks, @ to link docs or people" },
+    1: { id: 1, tag: "h1", placeholder: "Type to create a large heading", name: "Heading 1" },
+    2: { id: 2, tag: "h2", placeholder: "Type to create a medium heading", name: "Heading 2" },
+    3: { id: 3, tag: "p", placeholder: "Type / for blocks, @ to link docs or people", name: "Paragraph" },
 }
 
 export default textTypes;
